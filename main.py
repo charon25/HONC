98786da728018a8c685626575f864c5f8f730971
Paul Kern <paul.kern@insa-strasbourg.fr>
2021-06-13 11:05:17 +0200

    Icon

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pyghelper
 
 pygame.init()
 pygame.display.init()
-screen = pyghelper.Window.create(width=700, height=700, title='GMTK 2021')
+screen = pyghelper.Window.create(width=700, height=700, title='GMTK 2021', icon_path='resources/icon.png')
 
 from game import Game
 
